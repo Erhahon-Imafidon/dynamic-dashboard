ba8bd9aa45148c97eff120dd75760fe463d42428
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-14 17:10:25 +0100

    updated my packages

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,29 @@
 import Kodeguru from "../assets/kodeguru-Logo1.jpeg";
+import Kate from "../assets/kate.jpg";
 
 const Navbar = () => {
   return (
-    <section>
+    <section className="w-full p-5 flex">
       {/* Logo Section */}
       <div className="">
-        <img src={Kodeguru} alt="KodeguruDev Logo" className="w-16" />
+        <img src="logo.svg" alt="KodeguruDev Logo" className="" />
         <span>GurudevAdmin</span>
       </div>
       {/* ICONS */}
-      <div>
+      <div className="">
         <img src="/search.svg" alt="" />
         <img src="/app.svg" alt="" />
         <img src="/expand.svg" alt="" />
         {/* Notification */}
         <div>
           <img src="/notifications.svg" alt="" />
+          <span>1</span>
         </div>
         {/* Users  */}
-        <div></div>
+        <div>
+          <img src={Kate} alt="" className="h-9 w-9 rounded-full" />
+          <span></span>
+        </div>
         <img src="/settings.svg" alt="" />
       </div>
     </section>
