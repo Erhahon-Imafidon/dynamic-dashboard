6b62c8a0e9ccba453c9da12aa97eac65c3022e89
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-14 18:22:43 +0100

    Added prettier dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,22 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        // TEXT
-        "dark-color": "#2a3447",
-        "soft-color": "#ddd",
-        "main-color": "white",
-        // BG
-        "dark-bg": "#222b3c",
-        "soft-bg": "#384256",
-        "main-bg": "#2a3447",
-      },
-      fontFamily: {
-        inter: ["Inter", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   theme: {
+//     extend: {
+//       colors: {
+//         // TEXT
+//         "dark-color": "#2a3447",
+//         "soft-color": "#ddd",
+//         "main-color": "white",
+//         // BG
+//         "dark-bg": "#222b3c",
+//         "soft-bg": "#384256",
+//         "main-bg": "#2a3447",
+//       },
+//       fontFamily: {
+//         inter: ["Inter", "sans-serif"],
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
