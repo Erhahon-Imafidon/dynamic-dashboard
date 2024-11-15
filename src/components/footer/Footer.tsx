72201f92f74c61ca922d629d111744e8158ce024
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-15 02:43:38 +0100

    implemented ui for footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,5 +1,12 @@
+import './footer.scss';
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <div className="footer">
+            <span>kodeguru</span>
+            <span>&copy; kodeguru admin dashboard</span>
+        </div>
+    );
 };
 
 export default Footer;
