565fab324730df867c95d84effe7e22df117b31e
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-21 04:18:43 +0100

    Added contents to the Single Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import { Home, Users, Products } from './pages/index.tsx';
+import { Home, Users, Products, User, Product } from './pages/index.tsx';
 import Layout from './components/layout/Layout.tsx';
 
 const App = () => {
@@ -8,7 +8,9 @@ const App = () => {
             <Route path="/" element={<Layout />}>
                 <Route path="/" element={<Home />} />
                 <Route path="users" element={<Users />} />
+                <Route path="users/:id" element={<User />} />
                 <Route path="products" element={<Products />} />
+                <Route path="products/:id" element={<Product />} />
             </Route>
         </Routes>
     );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Home from './home/Home.tsx';
 import Users from './users/Users.tsx';
+import User from './user/User.tsx';
 import Products from './products/Products.tsx';
+import Product from './product/Product.tsx';
 
-export { Home, Users, Products };
+export { Home, Users, Products, User, Product };
