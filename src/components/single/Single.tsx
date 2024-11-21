df86d040bb57342468cd009a770fd7478054c133
Erhahon Imafidon <mafiosco4@gmail.com>
2024-11-21 05:29:40 +0100

    Added the types to the single components

--- src/components/single/Single.tsx
+++ src/components/single/Single.tsx
@@ -55,84 +55,82 @@ const data = [
     },
 ];
 
-const Single = () => {
+type SingleProps = {
+    id: number;
+    img?: string;
+    title: string;
+    info: string;
+    chart?: {
+        datakeys: { name: string; color: string }[];
+        data: object[];
+    };
+    activities: {
+        title: string;
+        time: string;
+    };
+};
+
+const Single = ({ id, img, title, info, chart, activities }: SingleProps) => {
     return (
         <div className="single">
             <div className="view">
                 <div className="info">
                     <div className="topInfo">
-                        <img src={Avatar} alt="" />
-                        <h1>John Doe</h1>
+                        {img && <img src={img || Avatar} alt="" />}
+                        <h1>{title}</h1>
                         <button>Update</button>
                     </div>
                     <div className="details">
-                        <div className="item">
-                            <span className="itemTitle">Username:</span>
-                            <span className="itemValue">John Doe</span>
-                        </div>
-                        <div className="item">
-                            <span className="itemTitle">Username:</span>
-                            <span className="itemValue">John Doe</span>
-                        </div>
-                        <div className="item">
-                            <span className="itemTitle">Username:</span>
-                            <span className="itemValue">John Doe</span>
-                        </div>
+                        {Object.entries(info).map((item) => (
+                            <div className="item" key={item[0]}>
+                                <span className="itemTitle">{item[0]}</span>
+                                <span className="itemValue">{item[1]}</span>
+                            </div>
+                        ))}
                     </div>
                     <div className="item"></div>
                 </div>
                 <hr />
-                <div className="chart">
-                    <ResponsiveContainer width="100%" height="100%">
-                        <LineChart
-                            width={500}
-                            height={300}
-                            data={data}
-                            margin={{
-                                top: 5,
-                                right: 30,
-                                left: 20,
-                                bottom: 5,
-                            }}
-                        >
-                            <XAxis dataKey="name" />
-                            <YAxis />
-                            <Tooltip />
-                            <Legend />
-                            <Line
-                                type="monotone"
-                                dataKey="pv"
-                                stroke="#8884d8"
-                                activeDot={{ r: 8 }}
-                            />
-                            <Line
-                                type="monotone"
-                                dataKey="uv"
-                                stroke="#82ca9d"
-                            />
-                        </LineChart>
-                    </ResponsiveContainer>
-                </div>
+                {chart && (
+                    <div className="chart">
+                        <ResponsiveContainer width="100%" height="100%">
+                            <LineChart
+                                width={500}
+                                height={300}
+                                data={chart?.data || data}
+                                margin={{
+                                    top: 5,
+                                    right: 30,
+                                    left: 20,
+                                    bottom: 5,
+                                }}
+                            >
+                                <XAxis dataKey="name" />
+                                <YAxis />
+                                <Tooltip />
+                                <Legend />
+
+                                {chart.datakeys.map(
+                                    (datakey: {
+                                        name: string;
+                                        color: string;
+                                    }) => (
+                                        <Line
+                                            type="monotone"
+                                            dataKey={datakey.name}
+                                            stroke={datakey.color}
+                                            activeDot={{ r: 8 }}
+                                        />
+                                    )
+                                )}
+                            </LineChart>
+                        </ResponsiveContainer>
+                    </div>
+                )}
             </div>
             <div className="activities">
                 <h2>Latest Activities</h2>
                 <ul>
-                    <li>
-                        <div>
-                            <p>
-                                John Doe purchased Playstation 5 Digital Edition
-                            </p>
-                            <time>3 days ago</time>
-                        </div>
-                    </li>
-                    <li>
-                        <div>
-                            <p>
-                                John Doe purchased Playstation 5 Digital Edition
-                            </p>
-                            <time>3 days ago</time>
-                        </div>
-                    </li>
                     <li>
                         <div>
                             <p>
